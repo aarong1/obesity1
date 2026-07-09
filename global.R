ca34eed647a3edc42d3b0723774564c762e31184
Aaron Gorman <gormanaa@tcd.ie>
2026-07-09 14:36:46 +0100

    fix:manifest json

--- global.R
+++ global.R
@@ -33,24 +33,35 @@ source('1_2_utils/main_configuration.R')
 
 dir.create('./3_pre_main/intermediate_populations')
 
+
+
+download.file(
+  "https://storage.googleapis.com/time_one_population_w_deaths/populations/first_population.fst",
+  destfile = "./3_pre_main/intermediate_populations/first_population.fst",
+  mode = "wb"
+)
+ 
+first_population <- read.fst('3_pre_main/intermediate_populations/first_population.fst')
+ 
+
 download.file(
   "https://storage.googleapis.com/time_one_population_w_deaths/populations/initial_time_zero_population.fst",
   destfile = "./3_pre_main/intermediate_populations/initial_time_zero_population.fst",
   mode = "wb"
 )
-print('fine')
+ 
 initial_time_zero_population <- read.fst('3_pre_main/intermediate_populations/initial_time_zero_population.fst')
-print('fine')
+ 
 
 download.file(
   "https://storage.googleapis.com/time_one_population_w_deaths/populations/base_population_w_physiological_and_modifiable_risk_factors.fst",
   destfile = "./3_pre_main/intermediate_populations/base_population_w_physiological_and_modifiable_risk_factors.fst",
   mode = "wb"
 )
-print('fine')
+ 
 base_population_w_physiological_and_modifiable_risk_factors <- read.fst('3_pre_main/intermediate_populations/base_population_w_physiological_and_modifiable_risk_factors.fst')
 
-print('fine')
+ 
 # download.file(
 #   "https://storage.googleapis.com/time_one_population_w_deaths/populations/full_history_past_populations.fst",
 #   destfile = "./3_pre_main/intermediate_populations/full_history_past_populations.fst",
@@ -63,9 +74,9 @@ download.file(
   destfile = "./3_pre_main/intermediate_populations/time_one_population.fst",
   mode = "wb"
 )
-print('fine')
+ 
 past_populations <- read.fst('./3_pre_main/intermediate_populations/time_one_population.fst')
-print('fine')
+ 
 
 past_populations$intervention = 'non-intervention'
 
